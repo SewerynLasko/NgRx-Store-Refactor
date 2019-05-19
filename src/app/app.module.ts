fc7567701b4b09720bf71d561e01fecb29a2a0c3
Seweryn <seweryn.lasko@gmail.com>
2019-05-19 13:40:20 +0200

    Custom Router State Serializers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, Routes } from '@angular/router';
 import { EffectsModule } from '@ngrx/effects';
+import { StoreRouterConnectingModule, RouterStateSerializer } from '@ngrx/router-store';
 import { MetaReducer, StoreModule } from '@ngrx/store';
 // not used in production
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { storeFreeze } from 'ngrx-store-freeze';
 // bootstrap
 import { AppComponent } from './containers/app/app.component';
-import { reducers } from './store';
+import { reducers, CustomSerializer } from './store';
 
 // this would be done dynamically with webpack for builds
 const environment = {
@@ -36,8 +37,12 @@ export const ROUTES: Routes = [
     StoreModule.forRoot(reducers, { metaReducers }), // reducers, config
     // instatiate Store
     EffectsModule.forRoot([]),
+    StoreRouterConnectingModule, // This will register ngrx/router which will update our store with routes
     environment.development ? StoreDevtoolsModule.instrument() : []
   ],
+  providers: [{ provide: RouterStateSerializer, useClass: CustomSerializer }],
+  // Provide (register) RouterStoreSerlializer and use our custom class Serializer
+  // When StoreRouterConnectingModule will internally use RouterStateSerializer token we replace it with our own
   declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
